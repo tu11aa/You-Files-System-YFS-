0011b5b05fb4770d6ddd4834053d1649403f56c0
Tu <thanhtuhr01@gmail.com>
2024-04-10 23:45:25 +0700

    added message class

---
+++ yfs/message.py
@@ -0,0 +1,55 @@
+import json
+
+class Message:
+    BROADCAST = 0
+    BROADCAST_REPONSE = 1
+    GET = 2
+    READ = 4
+    WRITE = 5
+    START_WRITING = 6
+    END_WRITING = 7
+
+    @classmethod
+    def from_dict(cls, message):
+        return Message(
+            message["sender"],
+            message["receiver"],
+            message["message"],
+            message["timestamps"],
+            message["vp"],
+            message["message_type"],
+        )
+    
+    @classmethod
+    def from_string(cls, message):
+        return Message.from_dict(json.loads(message))
+
+    def __init__(self, sender: str, receiver: str, message: str, timestamps: list, vp: dict, message_type: int) -> None:
+        self.sender = sender
+        self.receiver = receiver
+        self.message = message
+        self.timestamps = timestamps
+        self.vp = vp
+        self.message_type = message_type
+
+    def __str__(self) -> str:
+        return json.dumps(self.__dict__)
+    
+
+#how to use :))))
+
+#p1:
+p1_tp = [1, 0]
+p1_vp = {}
+m1 = Message("p1", "p2", "hello", p1_tp, p1_vp, Message.BROADCAST)
+#send
+p1_vp[m1.receiver] = m1.timestamps
+print(p1_vp)
+#--------------------------------------#
+p2_tp = [0, 0]
+p2_vp = {}
+
+m2 = str(m1)
+m2 = Message.from_string(m2)
+
+#compare m2.timestamp < m1.timestamp
