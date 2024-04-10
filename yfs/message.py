e92864cf1ee8e49960da110b8fa8749831c0df1d
Tu <thanhtuhr01@gmail.com>
2024-04-10 23:48:10 +0700

    just need to run main when message id called (demo only)

--- yfs/message.py
+++ yfs/message.py
@@ -35,21 +35,21 @@ class Message:
     def __str__(self) -> str:
         return json.dumps(self.__dict__)
     
+if __name__ == "__main__":
+    #how to use :))))
 
-#how to use :))))
+    #p1:
+    p1_tp = [1, 0]
+    p1_vp = {}
+    m1 = Message("p1", "p2", "hello", p1_tp, p1_vp, Message.BROADCAST)
+    #send
+    p1_vp[m1.receiver] = m1.timestamps
+    print(p1_vp)
+    #--------------------------------------#
+    p2_tp = [0, 0]
+    p2_vp = {}
 
-#p1:
-p1_tp = [1, 0]
-p1_vp = {}
-m1 = Message("p1", "p2", "hello", p1_tp, p1_vp, Message.BROADCAST)
-#send
-p1_vp[m1.receiver] = m1.timestamps
-print(p1_vp)
-#--------------------------------------#
-p2_tp = [0, 0]
-p2_vp = {}
+    m2 = str(m1)
+    m2 = Message.from_string(m2)
 
-m2 = str(m1)
-m2 = Message.from_string(m2)
-
-#compare m2.timestamp < m1.timestamp
+    #compare m2.timestamp < m1.timestamp
