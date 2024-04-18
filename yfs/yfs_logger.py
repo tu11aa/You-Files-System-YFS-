84c1ebaea8f02a5c15ae675d50e5ee96ac22a4ee
Tu <thanhtuhr01@gmail.com>
2024-04-18 16:05:17 +0700

    +added response command

--- yfs/yfs_logger.py
+++ yfs/yfs_logger.py
@@ -7,18 +7,24 @@ class YFSLogger:
 
     @classmethod
     def command_as_string(self, command):
+        if command < 0:
+            response = "RESPONSE_"
+        else:
+            response = ""
+        result = ""
         if command == MessageType.READ:
-            return "READ"
+            result = "READ"
         if command == MessageType.WRITE:
-            return "WRITE"
+            result = "WRITE"
         if command == MessageType.BROADCAST:
-            return "BROADCAST"
+            result = "BROADCAST"
         if command == MessageType.START_WRITING:
-            return "START_WRITING"
+            result = "START_WRITING"
         if command == MessageType.END_WRITING:
-            return "END_WRITING"
+            result = "END_WRITING"
         if command == MessageType.GET:
-            return "GET"
+            result = "GET"
+        return response + result
 
     def log(self, command, result):
         timestamp = datetime.now()
